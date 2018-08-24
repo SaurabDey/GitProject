42944e35a7817c6705f4b1fa17ada6e70592ecc1
SaurabDey <saurab.jeet@gmail.com>
2018-08-24 08:10:40 +0530

    My Initial Commit

---
+++ src/test/java/orgtest/com/GitProject/MyBrowserClass.java
@@ -0,0 +1,39 @@
+package orgtest.com.GitProject;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class MyBrowserClass 
+{
+	WebDriver driver;
+	@BeforeTest
+	public void bt()
+	{
+		System.setProperty("webdriver.chrome.driver", "Resource/chromedriver.exe");
+		driver= new ChromeDriver();
+		driver.get("https://www.facebook.com");
+		driver.manage().window().maximize();
+	}
+	
+	@Test
+	public void t()
+	{
+		WebElement username= driver.findElement(By.id("email"));
+		username.sendKeys("Saurab@gmail.com");
+		WebElement password= driver.findElement(By.id("pass"));
+		password.sendKeys("12345");
+		WebElement loginBtn= driver.findElement(By.xpath("//input[@type='submit']"));
+		loginBtn.click();;
+	}
+	
+	@AfterTest
+	public void at()
+	{
+		driver.quit();
+	}
+}
