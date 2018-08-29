53c6faa5f23590300815c89e7f98037aaad72a28
SaurabDey <saurab.jeet@gmail.com>
2018-08-29 09:08:20 +0530

    Added Extent functionality

--- src/main/java/orgtest/com/GitProject/FacebookLoginPage.java
+++ src/main/java/orgtest/com/GitProject/FacebookLoginPage.java
@@ -1,5 +1,11 @@
 package orgtest.com.GitProject;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -7,34 +13,77 @@ import org.openqa.selenium.support.How;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.Assert;
 
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.MediaEntityBuilder;
+import com.aventstack.extentreports.MediaEntityModelProvider;
+
 public class FacebookLoginPage {
 
 	WebDriver driver;
-
+	 ExtentTest test;
 	@FindBy(how=How.ID, using= "email")
 	WebElement userName;
 
 	@FindBy(id = "pass")
 	WebElement passWord;
 
-	@FindBy(xpath = "//input[@type='submit']")
+	@FindBy(xpath = "//input[@type='submi']")
 	WebElement loginButton;
 
 	public FacebookLoginPage(WebDriver driver) {
-		this.driver = driver;
+		this.driver = driver;	
+		PageFactory.initElements(driver, this);
+	}
+
+	public FacebookLoginPage(WebDriver driver, ExtentTest test) {
 		
+        this.driver = driver;
+		this.test=test;
 		PageFactory.initElements(driver, this);
 	}
 
 	public void loginMethod() {
 
-		userName.sendKeys("Saurab@gmail.com");
+		try {
+			userName.sendKeys("Saurab@gmail.com");
+			test.pass("Entered userName in Login Page");
+			
+			passWord.sendKeys("12345");
+			test.pass("Entered password in Login Page");
 
-		passWord.sendKeys("12345");
-
-		loginButton.click();
+			loginButton.click();
+			test.pass("Clicked login button in Login Page");
+			
+			Assert.assertEquals(driver.getCurrentUrl(), "https://www.facebook.com/login.php?login_attempt=1&lwv=110");
+		} catch (Exception e) {
+			
+			try {
+				test.fail("LoginMethod failed in Login Page").addScreenCaptureFromPath(screenshot("screenshot1.png"));
+			} catch (IOException e1) {
+				e1.printStackTrace();
+			}
+			
+			MediaEntityModelProvider mediaModel;
+			try {
+				mediaModel = MediaEntityBuilder.createScreenCaptureFromPath(screenshot("screenshot2.png")).build();
+				test.fail("LoginMethod failed in Login Page using MediaEntityModelProvider", mediaModel);
+			} catch (IOException e1) {
+				
+				e1.printStackTrace();
+			}
 		
-		Assert.assertEquals(driver.getCurrentUrl(), "https://www.facebook.com/login.php?login_attempt=1&lwv=110");
+		}
+		
 	}
 
+	
+	public String screenshot(String screenShotName) throws IOException
+	{
+		TakesScreenshot ts = (TakesScreenshot)driver;
+		File source = ts.getScreenshotAs(OutputType.FILE);
+		String dest = System.getProperty("user.dir") +"\\Screens\\"+screenShotName+".png";
+		File destination = new File(dest);
+		FileUtils.copyFile(source, destination);
+		return dest;        
+	}
 }
--- src/test/java/orgtest/com/GitProject/MyBrowserClass.java
+++ src/test/java/orgtest/com/GitProject/MyBrowserClass.java
@@ -9,26 +9,61 @@ import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+import com.aventstack.extentreports.reporter.configuration.Theme;
+
 public class MyBrowserClass 
 {
 	WebDriver driver;
+	ExtentTest test ;
+	ExtentReports extent;
 	@BeforeTest
 	public void bt()
 	{
+		//-----ExtentHtmlReporter class
+		ExtentHtmlReporter htmlReporter = new ExtentHtmlReporter("SaurabGitJenkins.html");
+		htmlReporter.setAppendExisting(true);
+		htmlReporter.config().setChartVisibilityOnOpen(true);
+		htmlReporter.config().setDocumentTitle("Saurab - ExtentReports");
+		htmlReporter.config().setTheme(Theme.DARK);
+		
+		//----extent reports class
+		extent= new ExtentReports();
+		extent.attachReporter(htmlReporter);
+		//----extent test class
+		test= extent.createTest("MyFirstTest");
+		
+		test.pass("Automation Started");
+		test.log(Status.PASS, "Automation Started");
+		
 		System.setProperty("webdriver.chrome.driver", "Resource/chromedriver.exe");
 		driver= new ChromeDriver();
+		test.pass("Browser Started");
+		
 		driver.get("https://www.facebook.com");
+		test.pass("URL launched successfully");
 		driver.manage().window().maximize();
 		
 		Assert.assertEquals(driver.getTitle(), "Facebook – log in or sign up");
+		test.pass("We are in the Login Page");
 	}
 	
 	@Test
 	public void t()
 	{
-		FacebookLoginPage log= new FacebookLoginPage(driver);
+		try{
+		test.pass("Login Started");
+		FacebookLoginPage log= new FacebookLoginPage(driver,test);
 		log.loginMethod();
-		
+		test.pass("Login successfully complted");
+		}
+		catch(Exception e)
+		{
+		test.fail("Something went wrong with FacebookLoginPage "+ e);
+		}
 //		FaceBookDashboardPage po= new FaceBookDashboardPage(driver);
 //		po.postMethd();
 	}
@@ -37,5 +72,7 @@ public class MyBrowserClass
 	public void at()
 	{
 		driver.quit();
+		test.pass("End of execution");
+		extent.flush();
 	}
 }
